c66abdc23f619f46113839bd519d7d1767bc4aec
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-26 16:20:03 -0500

    first addKeyAndValue attempt, using repo as ref

--- reduce.js
+++ reduce.js
@@ -1,11 +1,3 @@
-/*
-Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
-
-Examples:
-    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
-    extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
-*/
-
 function extractValue(arr, key) {
   return arr.reduce(function (acc, nextValue) {
     acc.push(nextValue[key]);
@@ -13,25 +5,15 @@ function extractValue(arr, key) {
   }, []); //no need to initialize new empty array, simply use second parameter of reduce for []
 }
 
-/*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
-
-Examples:
-    vowelCount('Elie') // {e:2,i:1};
-    vowelCount('Tim') // {i:1};
-    vowelCount('Matt') // {a:1})
-    vowelCount('hmmm') // {};
-    vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
-*/
-
 function vowelCount(str) {
   const vowels = "aeiou"; //copied over previous vowel counting code
   strArr = str.split("");
   return strArr.reduce(function (acc, nextValue) {
-    let lowerCase = nextValue.toLowerCase();
+    let lowerCase = nextValue.toLowerCase(); //lowercasing each letter
     if (vowels.indexOf(lowerCase) !== -1) {
+      //setting up vowel search
       if (acc[lowerCase]) {
-        acc[lowerCase]++;
+        acc[lowerCase]++; //increasing count of vowel
       } else {
         acc[lowerCase] = 1;
       }
@@ -55,7 +37,12 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+  arr.reduce(function (acc, nextValue) {
+    acc[key] = value;
+  }, []);
+  return arr;
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
